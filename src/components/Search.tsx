ce0b86a923eaaa2cfec05675991c27feb05518a0
hcarine <hammescarine@gmail.com>
2021-01-24 20:33:33 +0100

    start search component

---
+++ src/components/Search.tsx
@@ -0,0 +1,8 @@
+export const Search = () => {
+    return (
+        <form>
+           <input type="text" placeholder="Type here the value to search"/>
+           <input type="submit" value="Start search" />
+        </form>
+    )
+}
